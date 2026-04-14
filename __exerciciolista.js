17162af564c79f4c0d66fda5fc184a3a13e3a153
mariaclaraoliveira15-a11y <maria.clara.oliveira15@escola.pr.gov.br>
2026-04-14 09:04:20 -0300

    Add files via upload

---
+++ __exerciciolista.js
@@ -0,0 +1,28 @@
+----------------ATIVIDADES COM FOR ----------------
+1 EXIBA OS NUMEROS DE 1 A 10 USANDO FOR
+for(let i=1; i=<10; i++){
+    console.log(i);
+}
+//2//
+for(let i=11; i>=1; i--){
+
+}
+
+//3//
+for(let i=2; i=<20; i=i+2
+){console.log(i);}
+
+//4//
+for(let i=1; i=<10;)
+    console.log('5 x ${i}=${5*i}');
+
+//5//
+for(let i=1; i<=50; i++)
+    {console.log(i)}
+
+//6//
+for(let i=1; i<=8; i++; i=i+1)
+{console.log(i);}
+
+//7//
+for(let i=1; i<=15; i=i-1){console.log(i);}
